14fbb3e0696a9d064b0332377bddd38d304ef9a6
Jason Dsouza <jasonrdsouza@gmail.com>
2015-01-27 22:06:58 -0500

    Add flag to specify which directory to serve, and gofmt code

--- file-server/main.go
+++ file-server/main.go
@@ -1,26 +1,30 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "os"
-    "path/filepath"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 )
 
+// Allow users to specify which directory to serve, defaulting to the users Public directory
+var directory_to_serve = flag.String("dir", filepath.Join(os.Getenv("HOME"), "Public"), "the static directory to serve")
+
 func logRequests(h http.Handler) http.Handler {
-  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    //do stuff with the request here
-    log.Println("Request logged")
-    h.ServeHTTP(w, r)
-  })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//do stuff with the request here
+		log.Println("Request logged")
+		h.ServeHTTP(w, r)
+	})
 }
 
 func main() {
-  path_to_serve := filepath.Join(os.Getenv("HOME"), "Public")
-  port_to_serve := ":8080"
+	flag.Parse()
+	port_to_serve := ":8080"
 
-  log.Println("FileServer started on port", port_to_serve, "...")
-  log.Println("Serving directory: ", path_to_serve)
-  handler := logRequests(http.FileServer(http.Dir(path_to_serve)))
-  log.Fatal(http.ListenAndServe(port_to_serve, handler))
+	log.Println("FileServer started on port", port_to_serve, "...")
+	log.Println("Serving directory: ", *directory_to_serve)
+	handler := logRequests(http.FileServer(http.Dir(*directory_to_serve)))
+	log.Fatal(http.ListenAndServe(port_to_serve, handler))
 }
